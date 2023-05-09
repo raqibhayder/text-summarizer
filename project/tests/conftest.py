21b013583fb06391affb4f783416cc4c107b6f8a
Raqib Hayder <1992290+raqib-hayder@users.noreply.github.com>
2023-05-09 15:27:02 -0400

    Refactor and add db initialization logic

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -3,7 +3,7 @@ import os
 import pytest as pytest
 from starlette.testclient import TestClient
 
-from app import main
+from app.main import create_application
 from app.config import Settings, get_settings
 
 
@@ -14,10 +14,10 @@ def get_settings_override():
 @pytest.fixture(scope="module")
 def test_app():
     # set up
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
         # test
         yield test_client
 
     # tear down
-
