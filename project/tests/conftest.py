1c4e45822468c8fae981a06730bab34ed1e66243
Raqib Hayder <1992290+raqib-hayder@users.noreply.github.com>
2023-05-10 02:50:03 -0400

    Add code coverage, code formatting and code quality.

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -4,8 +4,8 @@ import pytest as pytest
 from starlette.testclient import TestClient
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.main import create_application
 from app.config import Settings, get_settings
+from app.main import create_application
 
 
 def get_settings_override():
